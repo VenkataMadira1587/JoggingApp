9d5d8b2319c28683c8bfa63394d400f4c756e04a
Vince Reddy <venkat.06453@gmail.com>
2014-10-16 12:15:04 +0100

    jogging app commit

---
+++ JoggingAppTests/JoggingAppTests.m
@@ -0,0 +1,34 @@
+//
+//  JoggingAppTests.m
+//  JoggingAppTests
+//
+//  Created by Venkat on 01/02/2014.
+//  Copyright (c) 2014 Venkat. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JoggingAppTests : XCTestCase
+
+@end
+
+@implementation JoggingAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
